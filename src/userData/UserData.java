68ade72e584dd5b6ca35d87a6fc24b9b49c72192
sbojja1 <sbojja1@murraystate.edu>
2015-05-27 07:37:01 -0500

    phase1

---
+++ src/userData/UserData.java
@@ -0,0 +1,263 @@
+package userData;
+
+import java.io.Serializable;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+@ManagedBean(name = "userData", eager = true)
+@SessionScoped
+public class UserData implements Serializable {
+
+   private static final long serialVersionUID = 1L;
+
+   
+   
+   private String cmpny;
+   private String fnm;
+   private String lnm;
+   private String phoneExt; 
+   private String mbl;
+   private String fax;
+   private String email;
+   private String co;
+   private String hno;
+   private String str1;
+   private String str2;
+   private String str3;
+   private String str4;
+   private String dstct;
+   private String cty;
+   private String pcode;
+   private String rgn;
+   private String cntry;
+   private String mpnm;
+   private String dept;
+   private String rmno;
+   private String flr;
+   private String bldg;
+   private String datein;
+   private String timein;
+   private String edateout;
+   private String etimeout;
+   private String dateout;
+   private String timeout;
+   private String vcat;
+   private String vmk;
+   private String vmod;
+   private String vhno;
+   private String parea;
+   private String loc;
+   
+public String getCmpny() {
+	return cmpny;
+}
+public void setCmpny(String cmpny) {
+	this.cmpny = cmpny;
+}
+public String getFnm() {
+	return fnm;
+}
+public void setFnm(String fnm) {
+	this.fnm = fnm;
+}
+public String getLnm() {
+	return lnm;
+}
+public void setLnm(String lnm) {
+	this.lnm = lnm;
+}
+public String getPhoneExt() {
+	return phoneExt;
+}
+public void setPhoneExt(String phoneExt) {
+	this.phoneExt = phoneExt;
+}
+public String getMbl() {
+	return mbl;
+}
+public void setMbl(String mbl) {
+	this.mbl = mbl;
+}
+public String getFax() {
+	return fax;
+}
+public void setFax(String fax) {
+	this.fax = fax;
+}
+public String getEmail() {
+	return email;
+}
+public void setEmail(String email) {
+	this.email = email;
+}
+public String getCo() {
+	return co;
+}
+public void setCo(String co) {
+	this.co = co;
+}
+public String getHno() {
+	return hno;
+}
+public void setHno(String hno) {
+	this.hno = hno;
+}
+public String getStr1() {
+	return str1;
+}
+public void setStr1(String str1) {
+	this.str1 = str1;
+}
+public String getStr2() {
+	return str2;
+}
+public void setStr2(String str2) {
+	this.str2 = str2;
+}
+public String getStr3() {
+	return str3;
+}
+public void setStr3(String str3) {
+	this.str3 = str3;
+}
+public String getStr4() {
+	return str4;
+}
+public void setStr4(String str4) {
+	this.str4 = str4;
+}
+public String getDstct() {
+	return dstct;
+}
+public void setDstct(String dstct) {
+	this.dstct = dstct;
+}
+public String getCty() {
+	return cty;
+}
+public void setCty(String cty) {
+	this.cty = cty;
+}
+public String getPcode() {
+	return pcode;
+}
+public void setPcode(String pcode) {
+	this.pcode = pcode;
+}
+public String getRgn() {
+	return rgn;
+}
+public void setRgn(String rgn) {
+	this.rgn = rgn;
+}
+public String getCntry() {
+	return cntry;
+}
+public void setCntry(String cntry) {
+	this.cntry = cntry;
+}
+public String getMpnm() {
+	return mpnm;
+}
+public void setMpnm(String mpnm) {
+	this.mpnm = mpnm;
+}
+public String getDept() {
+	return dept;
+}
+public void setDept(String dept) {
+	this.dept = dept;
+}
+public String getRmno() {
+	return rmno;
+}
+public void setRmno(String rmno) {
+	this.rmno = rmno;
+}
+public String getFlr() {
+	return flr;
+}
+public void setFlr(String flr) {
+	this.flr = flr;
+}
+public String getBldg() {
+	return bldg;
+}
+public void setBldg(String bldg) {
+	this.bldg = bldg;
+}
+public String getDatein() {
+	return datein;
+}
+public void setDatein(String datein) {
+	this.datein = datein;
+}
+public String getTimein() {
+	return timein;
+}
+public void setTimein(String timein) {
+	this.timein = timein;
+}
+public String getEdateout() {
+	return edateout;
+}
+public void setEdateout(String edateout) {
+	this.edateout = edateout;
+}
+public String getEtimeout() {
+	return etimeout;
+}
+public void setEtimeout(String etimeout) {
+	this.etimeout = etimeout;
+}
+public String getDateout() {
+	return dateout;
+}
+public void setDateout(String dateout) {
+	this.dateout = dateout;
+}
+public String getTimeout() {
+	return timeout;
+}
+public void setTimeout(String timeout) {
+	this.timeout = timeout;
+}
+public String getVcat() {
+	return vcat;
+}
+public void setVcat(String vcat) {
+	this.vcat = vcat;
+}
+public String getVmk() {
+	return vmk;
+}
+public void setVmk(String vmk) {
+	this.vmk = vmk;
+}
+public String getVmod() {
+	return vmod;
+}
+public void setVmod(String vmod) {
+	this.vmod = vmod;
+}
+public String getVhno() {
+	return vhno;
+}
+public void setVhno(String vhno) {
+	this.vhno = vhno;
+}
+public String getParea() {
+	return parea;
+}
+public void setParea(String parea) {
+	this.parea = parea;
+}
+public String getLoc() {
+	return loc;
+}
+public void setLoc(String loc) {
+	this.loc = loc;
+}
+
+}
